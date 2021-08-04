aa00a4ca665e9c02fe7ba2efd65f656b8c3a19ff
Ian Clark <ianclark226@gmail.com>
2021-08-04 14:10:03 -0400

    typos

---
+++ script.js
@@ -0,0 +1,15 @@
+// const loader = document.querySelector('.loader');
+// const body = document.getElementById('body');
+
+// function  init() {
+//     setTimeout(() => {
+//         loader.style.opacity = 0;
+//         loader.style.display = 'none';
+
+//         body.style.display = 'block';
+//         setTimeout(() => (body.style.opacity = 1), 50);
+        
+//     }, 4000)
+// }
+
+// init();
